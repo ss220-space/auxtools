3fc3817b3ab1c7772fb17a623d6094d83d6b9d2d
willox <me@wiox.me>
2021-06-12 01:51:41 +0000

    Deploying to docs from  @ 15488a9c49a037ecbb770a7a862202421bacb8e3 🚀

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["auxtools"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"auxtools/struct.Value.html\" title=\"struct auxtools::Value\">Value</a>","synthetic":false,"types":["auxtools::value::Value"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
