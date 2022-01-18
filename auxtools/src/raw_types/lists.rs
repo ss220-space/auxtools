cc0bb2716624b124b04dd6062486d5fb1c1cbf3d
Dimach <dimach2142@gmail.com>
2022-01-18 09:13:32 +0200

    Fix AssociativeListEntry

--- auxtools/src/raw_types/lists.rs
+++ auxtools/src/raw_types/lists.rs
@@ -4,28 +4,42 @@ use super::values;
 #[derive(Copy, Clone, Debug)]
 pub struct ListId(pub u32);
 
-#[repr(C)]
+#[repr(u8)]
 #[allow(unused)]
-enum Color {
+#[derive(Debug)]
+pub enum Color {
 	Red = 0,
 	Black = 1,
 }
 
 #[repr(C)]
+#[derive(Debug)]
 pub struct AssociativeListEntry {
-	key: values::Value,
-	value: values::Value,
-	color: Color,
-	left: *mut AssociativeListEntry,
-	right: *mut AssociativeListEntry,
+	pub key: values::Value,
+	pub value: values::Value,
+	pub right: *mut AssociativeListEntry,
+	pub left: *mut AssociativeListEntry,
+	pub parent: *mut AssociativeListEntry,
+	pub color: Color,
+	pub padding:[u8;3]
 }
 
 #[repr(C)]
+#[derive(Debug)]
 pub struct List {
 	pub vector_part: *mut values::Value,
 	pub assoc_part: *mut AssociativeListEntry,
 	pub allocated: u32,
 	pub length: u32,
 	pub refcount: u32,
-	unknown: u32,
+	pub unknown: *mut ValueArrayData,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct ValueArrayData {
+	pub value_pointer: *mut values::Value,
+	pub unkn1: u32,
+	pub size: usize,
+	pub unkn2: u32,
 }
